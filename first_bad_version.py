8c904c7b52419cf9e83ae3eda8278dbe86f51ff9
caglayurdasal <caglayurdasal@gmail.com>
2022-07-10 18:34:47 +0300

    Add driver code and update main function

--- first_bad_version.py
+++ first_bad_version.py
@@ -1,22 +1,26 @@
 class Solution():
-    def firstBadVersion(self, versions: list) -> int:
+    def isBadVersion(self, n: int) -> int:
+        if n == 0:
+            return False
+        return True
+
+    def firstBadVersion(self, n: int, versions: list) -> int:
+        # 0 0 0 1 1 1 1 1 1
+        # 1 2 3 4 5 6 7 8 9
         low = 0
-        high = len(versions) - 1
-        while (low < high):
+        high = n - 1
+        while low < high:
             mid = (low + high) // 2
-            if versions[mid] > 0:
-                low = mid + 1
-            else:
+            if self.isBadVersion(versions[mid]):
                 high = mid
-        if versions[low] == 0:
-            return low
-        else:
-            return -1
+            else:
+                low = mid + 1
+        return low + 1
 
 
 def main():
     s = Solution()
-    print(s.firstBadVersion([1, 1, 1, 1, 1, 0, 0, 0]))
+    print("First bad version was published on the " + str(s.firstBadVersion(9, [0, 0, 0, 1, 1, 1, 1, 1, 1])) + ". day.")
 
 
 if __name__ == '__main__':

c07e169732c55fb6fd09acad7ef7ad42f187d1cb
caglayurdasal <caglayurdasal@gmail.com>
2022-07-10 17:59:16 +0300

    Update class and add driver code

---
+++ first_bad_version.py
@@ -0,0 +1,11 @@
+class Solution():
+    def firstBadVersion(self, versions: list) -> int:
+    
+
+def main():
+    s = Solution()
+    print(s.firstBadVersion(['G', 'G', 'G', 'G', 'B', 'B', 'B']))
+
+
+if __name__ == '__main__':
+    main()

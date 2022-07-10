afc89bed88cf94eb363bc9fca93eda76077a614e
caglayurdasal <caglayurdasal@gmail.com>
2022-07-10 22:30:25 +0300

    Add test cases

--- search_insert_pos.py
+++ search_insert_pos.py
@@ -1,10 +1,23 @@
 class Solution:
-    def searchInsert(self, nums: list, target: int) -> int:
-        pass
+
+    @staticmethod
+    def searchInsert(nums: int, target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid
+        if nums[low] >= target:
+            return low
+        else:
+            return low + 1
 
 
 def main():
-    s = Solution
+    s = Solution()
     print(s.searchInsert([1, 3, 5, 6], 5))
     print(s.searchInsert([1, 3, 5, 6], 2))
     print(s.searchInsert([1, 3, 5, 6], 7))

e0f3b56a274ebe27d8c77fa8ba25f97e45e1cc91
caglayurdasal <caglayurdasal@gmail.com>
2022-07-10 18:05:29 +0300

    Update class and add driver code

--- first_bad_version.py
+++ first_bad_version.py
@@ -1,10 +1,22 @@
 class Solution():
     def firstBadVersion(self, versions: list) -> int:
-    
+        low = 0
+        high = len(versions) - 1
+        while (low < high):
+            mid = (low + high) // 2
+            if versions[mid] > 0:
+                low = mid + 1
+            else:
+                high = mid
+        if versions[low] == 0:
+            return low
+        else:
+            return -1
+
 
 def main():
     s = Solution()
-    print(s.firstBadVersion(['G', 'G', 'G', 'G', 'B', 'B', 'B']))
+    print(s.firstBadVersion([1, 1, 1, 1, 1, 0, 0, 0]))
 
 
 if __name__ == '__main__':

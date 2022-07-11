38ed8555a8846af3e5863b0c170d5c707935451f
caglayurdasal <caglayurdasal@gmail.com>
2022-07-11 13:34:44 +0300

    Update function twoSum

--- two_sum.py
+++ two_sum.py
@@ -12,7 +12,10 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
 
 class Solution:
     def twoSum(self, nums: list, target: int) -> list:
-        pass
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i, j
 
 
 def main():
@@ -20,3 +23,6 @@ def main():
     print(s.twoSum([2, 7, 11, 15], 9))
     print(s.twoSum([3, 2, 4], 6))
 
+
+if __name__ == '__main__':
+    main()

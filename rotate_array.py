6259cdc0a5967a66fcca9d069a7894bc7ef23803
caglayurdasal <caglayurdasal@gmail.com>
2022-07-11 12:34:57 +0300

    Add comments

--- rotate_array.py
+++ rotate_array.py
@@ -5,8 +5,7 @@ where k is non-negative.
 
 
 class Solution:
-    @staticmethod
-    def rotate(nums: int, k: int) -> None:
+    def rotate(self, nums: list, k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
@@ -15,19 +14,18 @@ class Solution:
         k=3
         5,6,7,1,2,3,4
         '''
-        k = k % len(nums)
-        temp = []
-        for i in range(len(nums)):
-            temp.append(nums[i])
-        for i in range(len(nums)):
-            if (i + k) >= len(nums):
-                nums[(i + k) - len(nums)] = temp[i]
-            else:
-                nums[i + k] = temp[i]
-        return nums
+        # In case k is bigger than size of array, get remainder to
+        # iterate the list
+        k %= len(nums)
+
+        return nums[len(nums)-k:]+nums[:len(nums)-k]
+
+def main():
+    s = Solution()
+    print(s.rotate([-1, -100, 3, 99], 2))
+    print(s.rotate([1, 2, 3, 4, 5, 6, 7], 3))
+    print(s.rotate([1, 3, 5, 7, 9], 2))
 
 
-s = Solution()
-print(s.rotate([-1, -100, 3, 99], 2))
-print(s.rotate([1, 2, 3, 4, 5, 6, 7], 3))
-print(s.rotate([1, 3, 5, 7, 9], 2))
+if __name__ == '__main__':
+    main()

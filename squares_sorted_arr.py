1820f6c83507a5d20de9aa3069a53406aea0f79d
caglayurdasal <caglayurdasal@gmail.com>
2022-07-11 12:07:22 +0300

    Add comments

--- squares_sorted_arr.py
+++ squares_sorted_arr.py
@@ -6,10 +6,11 @@ return an array of the squares of each number sorted in non-decreasing order.
 
 class Solution:
     def sortedSquares(self, nums: list) -> list:
-
+        # Get squares
         for i in range(len(nums)):
             nums[i] = nums[i] * nums[i]
 
+        # Sort array
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] >= nums[j]:

1ea2c48484ff1230960fa8fb7aedaf3e64515f5f
caglayurdasal <caglayurdasal@gmail.com>
2022-07-11 11:58:00 +0300

    Add problem description and function prototypes

---
+++ squares_sorted_arr.py
@@ -0,0 +1,19 @@
+"""
+Given an integer array nums sorted in non-decreasing order,
+return an array of the squares of each number sorted in non-decreasing order.
+"""
+
+
+class Solution:
+    def sortedSquares(self, nums: list) -> list:
+        pass
+
+
+def main():
+    s = Solution()
+    print(s.sortedSquares([-4, -1, 0, 3, 10]))
+    print(s.sortedSquares([-7, -3, 2, 3, 11]))
+
+
+if __name__ == '__main__':
+    main()
